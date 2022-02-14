81facebc28306bb40ebce5669da94a7828069fca
Mykyta Medvediev <arlekin8881@gmail.com>
2022-02-13 21:32:14 -0800

    Ok, webpack compiled everything fine this time, and it works. The only slight issue I noticed is that unless you divide your screen into halves, open a console, and only then click on a red tent, the coordinates are not quite right. It happens because as you change the proportions of the screen, image is resized along with it, changing the coordinates of the tent. Therefore, the image has to be static in all proportions. Otherwise, everything seems to be working.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(js|jsx)$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
+};
