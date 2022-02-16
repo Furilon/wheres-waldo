1c9dc06a351658500dc1c50b2b4a1cbf848ca26f
Mykyta Medvediev <arlekin8881@gmail.com>
2022-02-16 15:23:59 -0800

    Fixed the issue with the picture.

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import img0 from './assets/the-loc-nar.314cec99.jpg'
 
 const Game = (props) => {
     return (
         <div id="game" onClick={props.gameFunc}>
-            
+            <img src={img0} alt="sgds" />
         </div>
     );
 }
